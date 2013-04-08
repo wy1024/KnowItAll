a108458278eb18503ced3e570f4c68b3edcc38eb
Michael Schmitz <michael@schmitztech.com>
2013-04-08 09:36:02 -0700

    Extend App.

--- src/main/scala/edu/knowitall/example/Extractor.scala
+++ src/main/scala/edu/knowitall/example/Extractor.scala
@@ -6,13 +6,11 @@ import edu.knowitall.ollie.Ollie
 import edu.knowitall.tool.parse.StanfordParser
 import java.util.Scanner
 
-object Extractor {
-	def main(args: Array[String]){
-		val ollie = new Ollie() 
-		val parser = new StanfordParser()
-		for (line <- io.Source.stdin.getLines) {
-			val graph = parser(line)	
-			println(ollie(graph))
-		}	
-	}
+object Extractor extends App {
+  val ollie = new Ollie() 
+  val parser = new StanfordParser()
+  for (line <- io.Source.stdin.getLines) {
+    val graph = parser(line)	
+    println(ollie(graph))
+  }	
 }
